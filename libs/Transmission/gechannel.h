9fd54634bf240a18c0653e5406765e3730263cdb
Karol <280913@student.pwr.edu.pl>
2025-01-13 16:23:06 +0100

    add simulateInterference in channel as base virtual function

--- libs/Transmission/gechannel.h
+++ libs/Transmission/gechannel.h
@@ -3,14 +3,23 @@
 
 #include <channel.h>
 
-
 class GEChannel : public Channel
 {
 public:
-    GEChannel();
-    virtual std::vector<bool> simulateInterference(std::vector<bool> & codeword) override;
+    GEChannel(unsigned int seed, double transProbabilityInBadState, double transProbabilityInGoodState, std::pair<double, double> stateTransProb);
     virtual void applyInterference(std::vector<bool> & codeword) override;
     virtual Channel * clone() const  override;
+private:
+    inline void noiseOnChannelState();
+    inline void noiseOnCodeword(bool & bit);
+private:
+    enum State : bool{ BAD = 0, GOOD = 1 };
+    State state = GOOD;
+    std::pair<double, double> stateTransProb;   //first =? from Good to Bad, second from bad to good
+    double transProbabilityInBadState;
+    double transProbabilityInGoodState;
+
+
 };
 
 #endif // GECHANNEL_H
